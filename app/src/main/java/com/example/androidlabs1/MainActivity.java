23fd16013e1501562765494255fee42bdd2d1a81
Senya <yuliktsv@gmail.com>
2020-06-15 23:07:16 -0400

    lab3 layout created

--- app/src/main/java/com/example/androidlabs1/MainActivity.java
+++ app/src/main/java/com/example/androidlabs1/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.androidlabs1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,14 +19,83 @@ import android.widget.Toast;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText emailField;
+    SharedPreferences sp;
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_lab3);
 
-        TextView txt = findViewById(R.id.top1);
-        Button btn = findViewById(R.id.buttom1);
+        emailField = (EditText)findViewById(R.id.edt1);
+        sp = getSharedPreferences("FileName", Context.MODE_PRIVATE);
+        String savedString = sp.getString("ReserveName", "Default value");
+
+        emailField.setHint(savedString);
+
+        loginBtn = (Button)findViewById(R.id.login);
+        loginBtn.setOnClickListener( c -> {
+
+            Intent profilePage = new Intent(MainActivity.this, ProfileActivity.class);
+            //Give directions to go from this page, to SecondActivity
+            EditText et = (EditText)findViewById(R.id.edt2);
+
+            profilePage.putExtra("emailTyped", et.getText().toString());
+
+            //Now make the transition:
+            startActivityForResult( profilePage, 345);
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //get an editor object
+        SharedPreferences.Editor editor = sp.edit();
+
+        //save what was typed under the name "ReserveName"
+        String whatWasTyped = emailField.getText().toString();
+        editor.putString("ReserveName", whatWasTyped);
+
+        //write it to disk:
+        editor.commit();
+    }
+}
+}
+//    SharedPreferences prefs = null;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main_lab3);
+//
+//        TextView txt = findViewById(R.id.top1);
+//        TextView txt2 = findViewById(R.id.top2);
+//        EditText editText = findViewById(R.id.edt1);
+//        EditText editText2 = findViewById(R.id.edt2);
+//
+//
+//        prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
+//
+//
+//        String savedString = prefs.getString("ReserveName", "Default Value");
+//
+//       editText.setText(savedString);
+//        Button btn = findViewById(R.id.login);
+//
+//            btn.setOnClickListener(bt -> saveSharedPrefs(editText.getText().toString()));
+//
+//    }
+//
+//    private void saveSharedPrefs(String stringToSave) {
+//        SharedPreferences.Editor editor = prefs.edit();
+//        editor.putString("ReserveName", stringToSave);
+//        editor.commit();
+//    }
+//
+//}
+        /*Button btn = findViewById(R.id.buttom1);
         CheckBox ch = findViewById(R.id.chb);
         ImageButton imageButton = findViewById(R.id.img);
         Switch swt = findViewById(R.id.swt);
@@ -41,7 +113,8 @@ public class MainActivity extends AppCompatActivity {
                     .setAction("Undo", v -> swt.setChecked(false))
                     .show();
 
-        });
+        });*/
 
-    }
-}
+
+
+
---
+++ app/src/main/java/com/example/androidlabs1/ProfileActivity.java
@@ -0,0 +1,77 @@
+package com.example.androidlabs1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageButton takePictureBtn;
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_profile);
+
+        // get the intent that got us here
+        Intent loginPage = getIntent();
+
+        String emailTyped = loginPage.getStringExtra("emailTyped");
+
+        //Put the string that was sent from FirstActivity into the edit text:
+        EditText enterText = (EditText) findViewById(R.id.EmailEditText);
+        enterText.setText(emailTyped);
+
+        takePictureBtn = (ImageButton) findViewById(R.id.ImageButton);
+        takePictureBtn.setOnClickListener(c -> {
+
+
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+        });
+        Log.d(ACTIVITY_NAME, "In function: onCreate()");
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            takePictureBtn.setImageBitmap(imageBitmap);
+        }
+        Log.d(ACTIVITY_NAME, "In function: onActivityResult()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(ACTIVITY_NAME, "In function: onStart()");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(ACTIVITY_NAME, "In function: onResume()");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(ACTIVITY_NAME, "In function: onPause()");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(ACTIVITY_NAME, "In function: onStop()");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(ACTIVITY_NAME, "In function: onDestroy()");
+    }
+}
