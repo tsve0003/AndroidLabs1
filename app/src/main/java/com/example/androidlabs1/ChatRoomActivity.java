d08c6492d0a5867c0f89f365c1095579b62f094d
Senya <yuliktsv@gmail.com>
2020-07-23 00:32:29 -0400

    LAB 7 IS FINISHED

--- app/src/main/java/com/example/androidlabs1/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs1/ChatRoomActivity.java
@@ -21,6 +21,7 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
@@ -28,189 +29,148 @@ import java.util.List;
 
 public class ChatRoomActivity extends AppCompatActivity {
 
-    private ArrayList<MessageModel> listMessage = new ArrayList<>();
-    private Button sendBtn;
-    private Button receiveBtn;
-    private ListView listView;
-    private EditText editText;
-    private ChatAdapter adt;
-    DatabaseHelper dbOpener;
-    SQLiteDatabase db;
-    private static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
-    public static final String ITEM_SELECTED = "ITEM";
-    public static final String ITEM_POSITION = "POSITION";
-    public static final String ITEM_IS_SEND = "IS_SEND";
-    public static final String ITEM_ID = "ID";
-    private ArrayList<MessageModel> list_message = new ArrayList<>();
-    private AppCompatActivity parentActivity;
+
+        private static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+        public static final String ITEM_SELECTED = "ITEM";
+        public static final String ITEM_POSITION = "POSITION";
+        public static final String ITEM_IS_SEND = "IS_SEND";
+        public static final String ITEM_ID = "ID";
+        private ArrayList<MessageModel> list_message = new ArrayList<>();
+        private AppCompatActivity parentActivity;
+        DatabaseHelper dbHelper;
+        ListView theList;
 
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_room);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_chat_room);
+            MyListAdapter adpt = new MyListAdapter();
+            theList = findViewById(R.id.ListView);
+            boolean isTablet = findViewById(R.id.fragmentLocation) != null; //check if the FrameLayout is loaded
+            theList.setAdapter(adpt);
+            dbHelper = new DatabaseHelper(this);
+            viewMessages();
 
 
-        listView = (ListView) findViewById(R.id.ListView);
-        listView.setAdapter(adt = new ChatAdapter());
-        editText = (EditText) findViewById(R.id.ChatEditText);
-        dbOpener = new DatabaseHelper(this);
+            theList.setOnItemClickListener((list, item, position, id) -> {
+                //Create a bundle to pass data to the new fragment
+                Bundle dataToPass = new Bundle();
+                dataToPass.putString(ITEM_SELECTED, list_message.get(position).message);
+                dataToPass.putBoolean(ITEM_IS_SEND, list_message.get(position).isSend());
+                dataToPass.putInt(ITEM_POSITION, position);
+                dataToPass.putLong(ITEM_ID, list_message.get(position).id);
 
-        boolean isTablet = findViewById(R.id.fragmentLocation) != null;
-
-//        viewData();
-
-
-        listView.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
-        listView.setStackFromBottom(true);
-
-        sendBtn = (Button) findViewById(R.id.SendBtn);
-        receiveBtn = (Button) findViewById(R.id.ReceiveBtn);
-        listView.setOnItemClickListener((list, item, position, id) -> {
-            //Create a bundle to pass data to the new fragment
-            Bundle dataToPass = new Bundle();
-            dataToPass.putString(ITEM_SELECTED, list_message.get(position).message);
-            dataToPass.putBoolean(ITEM_IS_SEND, list_message.get(position).isSend());
-            dataToPass.putInt(ITEM_POSITION, position);
-            dataToPass.putLong(ITEM_ID, list_message.get(position).id);
-
-            if (isTablet) {
-                DetailsFragment dFragment = new DetailsFragment(); //add a DetailFragment
-                dFragment.setArguments(dataToPass); //pass it a bundle for information
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.fragmentLocation, dFragment) //Add the fragment in FrameLayout
-                        .commit(); //actually load the fragment.
-            } else //isPhone
-            {
-                Intent nextActivity = new Intent(ChatRoomActivity.this, EmptyActivity.class);
-                nextActivity.putExtras(dataToPass); //send data to next activity
-                startActivity(nextActivity); //make the transition
-            }
-        });
+                if (isTablet) {
+                    DetailsFragment dFragment = new DetailsFragment(); //add a DetailFragment
+                    dFragment.setArguments(dataToPass); //pass it a bundle for information
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragmentLocation, dFragment) //Add the fragment in FrameLayout
+                            .commit(); //actually load the fragment.
+                } else //isPhone
+                {
+                    Intent nextActivity = new Intent(ChatRoomActivity.this, EmptyActivity.class);
+                    nextActivity.putExtras(dataToPass); //send data to next activity
+                    startActivity(nextActivity); //make the transition
+                }
+            });
 
 
+            theList.setOnItemLongClickListener((parent, view, pos, id) -> {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle(getString(R.string.alertBuilderTitle));
+                builder.setMessage(getString(R.string.alertBuilderMsg1) + (pos + 1) + getString(R.string.alertBuilderMsg2) + id);
+                builder.setPositiveButton(getString(R.string.yes), (click, arg) -> {
+                    Log.e(ACTIVITY_NAME, "In function: onStart");
+                    dbHelper.deleteMessage(list_message.get(pos));
+                    list_message.clear();
+                    viewMessages();
+                    if (list_message.isEmpty()) list_message.add(null);
+                    adpt.notifyDataSetChanged();
+                    if (isTablet) {
+                        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragmentLocation);
+                        if (fragment != null)
+                            this.getSupportFragmentManager().beginTransaction().remove(fragment).commit();
+                    }
 
-        listView.setOnItemLongClickListener((parent, view, position, id) -> {
-            AlertDialog.Builder alert = new AlertDialog.Builder(this);
-            alert.setTitle(getResources().getString(R.string.alertBuilderTitle))
-                    .setMessage(getResources().getString(R.string.alertBuilderMsg1) + " " + position + "\n" + getResources().getString(R.string.alertBuilderMsg2) + " " + id)
+                });
+                builder.setNegativeButton(getString(R.string.no), (click, arg) -> {
+                });
+                builder.create().show();
+                return true;
+            });
 
-                    .setPositiveButton(getResources().getString(R.string.yes), (click, arg) -> {
-                        MessageModel selectedMessage = listMessage.get(position);
-                        dbOpener.deleteMessage(selectedMessage);
-                        //dbOpener.deleteMessage(position);
-                        listMessage.remove(position);
-                        adt.notifyDataSetChanged();
-                    })
-                    .setNegativeButton(getResources().getString(R.string.no), (click, arg) -> {
-                    });
-            if (listMessage.get(position).isSend) {
-                alert.setView(getLayoutInflater().inflate(R.layout.activity_main_send, null));
-            } else {
-                alert.setView(getLayoutInflater().inflate(R.layout.activity_main_receive, null));
-            }
+            EditText textView = findViewById(R.id.ChatEditText);
+            Button sendButton = findViewById(R.id.SendBtn);
 
+            sendButton.setOnClickListener(click -> {
+                String msg = textView.getText().toString();
+                dbHelper.insertData(msg, true);
+                list_message.clear();
+                viewMessages();
+                adpt.notifyDataSetChanged();
+                textView.setText("");
+            });
+            Button receiveButton = findViewById(R.id.receiveButton);
+            receiveButton.setOnClickListener(click -> {
+                String msg = textView.getText().toString();
+                dbHelper.insertData(msg, false);
+                list_message.clear();
+                viewMessages();
+                adpt.notifyDataSetChanged();
+                textView.setText("");
+            });
 
-            alert.create().show();
+        }
 
-            return true;
-        });
-
-        sendBtn.setOnClickListener(c -> {
-            String message = editText.getText().toString();
-//              MessageModel model = new MessageModel(message, true);
-//              listMessage.add(model);
-            //            listView.setAdapter(adt);
-//               editText.setText(null);
-//                adt.notifyDataSetChanged();
-            if (!message.equals("")){
-
-                dbOpener.insertData(message, true);
-                editText.setText("");
-                listMessage.clear();
-                viewData();
-            }
-        });
-
-        receiveBtn.setOnClickListener(c -> {
-            String message = editText.getText().toString();
-            if (!message.equals("")) {
-                dbOpener.insertData(message, false);
-                editText.setText("");
-                listMessage.clear();
-                viewData();
-            }
-//            MessageModel model = new MessageModel(message, false);
-//            listMessage.add(model);
-//            listView.setAdapter(adt);
-//            editText.setText(null);
-//           adt.notifyDataSetChanged();
-        });
-
-        listMessage.clear();
-        viewData();
-       // listView.setAdapter(adt = new ChatAdapter());
-    }
-
-        private void viewData(){
-            Cursor cursor = dbOpener.viewDataDb();
-
-            if (cursor.getCount() != 0){
-                while (cursor.moveToNext()){
-                    MessageModel model = new MessageModel(cursor.getString(1), cursor.getInt(2)==0?true:false, cursor.getInt(0));
-                    listMessage.add(model);
-//                    ChatAdapter adt = new ChatAdapter();
-                    listView.setAdapter(adt);
-                    adt.notifyDataSetChanged();
-
+        private void viewMessages() {
+            Cursor cursor = dbHelper.viewDataDb();
+            if (cursor.getCount() != 0) {
+                while (cursor.moveToNext()) {
+                    MessageModel msg = new MessageModel(cursor.getString(1), cursor.getInt(2) == 1 ? true : false, cursor.getLong(0));
+                    list_message.add(msg);
+                    theList.setAdapter(new MyListAdapter());
                 }
             }
-
-
-
-
-
-
-    }
-
-    class ChatAdapter extends BaseAdapter {
-
-        @Override
-        public int getCount() {
-            return listMessage.size();
         }
 
-        @Override
-        public MessageModel getItem(int position) {
-            return listMessage.get(position);
-        }
 
-        @Override
-        public long getItemId(int position) {
-            return getItem(position). getId();
-        }
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            MessageModel message = (MessageModel) getItem(position);
-//            View newView = convertView;
-            LayoutInflater inflater = getLayoutInflater();
 
-            if(convertView == null) {
-                if (message.isSend) {
-                    convertView = inflater.inflate(R.layout.activity_main_send, parent, false);
-                }
-                else {
-                    convertView = inflater.inflate(R.layout.activity_main_receive, parent, false);
-                }
+        private class MyListAdapter extends BaseAdapter {
+
+            public MyListAdapter() {
             }
-            TextView  messageText = (TextView)convertView.findViewById(R.id.textViewMessage);
-            messageText.setText(listMessage.get(position).message);
 
+            @Override
+            public int getCount() {
+                return list_message.size();
+            }
 
-            return convertView;
+            @Override
+            public MessageModel getItem(int position) {
+                return list_message.get(position);
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return list_message.get(position).getId();
+            }
+
+            @Override
+            public View getView(int position, View old, ViewGroup parent) {
+                TextView messageText;
+                if (getItem(position).isSend()) {
+                    old = getLayoutInflater().inflate(R.layout.activity_main_send, parent, false);
+                    messageText = old.findViewById(R.id.textViewMessage );
+
+                } else {
+                    old = getLayoutInflater().inflate(R.layout.activity_main_receive, parent, false);
+                    messageText = old.findViewById(R.id.textViewMessage);
+                }
+                messageText.setText(getItem(position).message);
+                return old;
+            }
         }
-
     }
-}
